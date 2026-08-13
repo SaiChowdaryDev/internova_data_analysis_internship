9aded113a71f895ee0ce7498523265c621667418
Sai Chowdary <saichowdary4081@gmail.com>
2026-08-13 19:26:46 +0530

    Add NumPy array creation and properties demonstration

--- Week-2-NumPy-Pandas/Task-01-NumPy-Introduction-Arrays/task1_numpy_arrays.py
+++ Week-2-NumPy-Pandas/Task-01-NumPy-Introduction-Arrays/task1_numpy_arrays.py
@@ -1,1 +1,19 @@
+import numpy as np
 
+numbers = np.array([10, 20, 30, 40, 50, 60, 70, 80, 90, 100])
+
+print("NumPy Array:")
+print(numbers)
+
+print("\nArray Shape:", numbers.shape)
+print("Array Size:", numbers.size)
+print("Array Data Type:", numbers.dtype)
+
+
+one_dimensional = np.array([1, 2, 3, 4, 5])
+print("\nOne-Dimensional Array:")
+print(one_dimensional)
+
+two_dimensional = np.array([[1, 2, 3], [4, 5, 6]])
+print("\nTwo-Dimensional Array:")
+print(two_dimensional)
