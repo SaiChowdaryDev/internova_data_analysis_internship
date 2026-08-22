313b09b7689ac56eaef81fb59949cb505f9e23fd
Sai Chowdary <saichowdary4081@gmail.com>
2026-08-22 13:34:15 +0530

    Add Departments table with initial data
    Create Departments table and insert initial data.

--- Week-3-SQL-Excel/Week-3-SQL-Excel/Task-04-SQL-Joins/departments.sql
+++ Week-3-SQL-Excel/Week-3-SQL-Excel/Task-04-SQL-Joins/departments.sql
@@ -1,1 +1,12 @@
+-- Departments Table
 
+CREATE TABLE Departments (
+    Branch_ID INT PRIMARY KEY,
+    Branch_Name VARCHAR(30)
+);
+
+INSERT INTO Departments VALUES
+(1, 'CSE'),
+(2, 'ECE'),
+(3, 'IT'),
+(4, 'EEE');
