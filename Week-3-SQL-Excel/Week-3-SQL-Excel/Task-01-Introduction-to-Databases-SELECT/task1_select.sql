73643fb2650923c96088c0912c52415873923a00
Sai Chowdary <saichowdary4081@gmail.com>
2026-08-22 10:02:12 +0530

    Update task1_select.sql

--- Week-3-SQL-Excel/Week-3-SQL-Excel/Task-01-Introduction-to-Databases-SELECT/task1_select.sql
+++ Week-3-SQL-Excel/Week-3-SQL-Excel/Task-01-Introduction-to-Databases-SELECT/task1_select.sql
@@ -1,1 +1,28 @@
+-- Task 1: Introduction to Databases & SELECT Statement
 
+
+-- 1. Display all records
+
+SELECT *
+FROM Students;
+
+
+-- 2. Select specific columns
+
+SELECT Name, Branch, Marks
+FROM Students;
+
+
+-- 3. Select student names and ages
+
+SELECT Name, Age
+FROM Students;
+
+
+-- 4. Use column aliases
+
+SELECT
+    Name AS Student_Name,
+    Branch AS Department,
+    Marks AS Score
+FROM Students;
