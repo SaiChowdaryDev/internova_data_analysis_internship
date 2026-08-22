dd3e35ba72bf3ac1e5a84cd944472727a0a829d2
Sai Chowdary <saichowdary4081@gmail.com>
2026-08-22 13:27:51 +0530

    Update students.sql

--- Week-3-SQL-Excel/Week-3-SQL-Excel/Task-03-GROUP-BY-HAVING/students.sql
+++ Week-3-SQL-Excel/Week-3-SQL-Excel/Task-03-GROUP-BY-HAVING/students.sql
@@ -1,1 +1,22 @@
+-- Task 3: Students Table
 
+CREATE TABLE Students (
+    Student_ID INT PRIMARY KEY,
+    Name VARCHAR(50),
+    Age INT,
+    Branch VARCHAR(20),
+    Marks INT
+);
+
+INSERT INTO Students (Student_ID, Name, Age, Branch, Marks)
+VALUES
+(101, 'Sai', 19, 'CSE', 85),
+(102, 'Rahul', 20, 'ECE', 78),
+(103, 'Priya', 19, 'CSE', 92),
+(104, 'Arjun', 21, 'IT', 88),
+(105, 'Anjali', 20, 'CSE', 75),
+(106, 'Kiran', 19, 'EEE', 81),
+(107, 'Sneha', 20, 'CSE', 90),
+(108, 'Ravi', 21, 'IT', 72),
+(109, 'Meena', 19, 'ECE', 86),
+(110, 'Vijay', 20, 'CSE', 95);
