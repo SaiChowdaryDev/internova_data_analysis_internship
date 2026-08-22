f565d7f8d10f5dd55f6d23ba7c15e447328c69d0
Sai Chowdary <saichowdary4081@gmail.com>
2026-08-22 13:35:04 +0530

    Implement SQL joins examples in task4_sql_joins.sql
    Added SQL queries demonstrating INNER JOIN, LEFT JOIN, and RIGHT JOIN operations.

--- Week-3-SQL-Excel/Week-3-SQL-Excel/Task-04-SQL-Joins/task4_sql_joins.sql
+++ Week-3-SQL-Excel/Week-3-SQL-Excel/Task-04-SQL-Joins/task4_sql_joins.sql
@@ -1,1 +1,37 @@
+-- Task 4: SQL Joins
 
+
+-- INNER JOIN
+
+SELECT
+    s.Student_ID,
+    s.Name,
+    d.Branch_Name,
+    s.Marks
+FROM Students s
+INNER JOIN Departments d
+ON s.Branch_ID = d.Branch_ID;
+
+
+-- LEFT JOIN
+
+SELECT
+    s.Student_ID,
+    s.Name,
+    d.Branch_Name,
+    s.Marks
+FROM Students s
+LEFT JOIN Departments d
+ON s.Branch_ID = d.Branch_ID;
+
+
+-- RIGHT JOIN
+
+SELECT
+    s.Student_ID,
+    s.Name,
+    d.Branch_Name,
+    s.Marks
+FROM Students s
+RIGHT JOIN Departments d
+ON s.Branch_ID = d.Branch_ID;
