63d750cd76674aa8fd858b850ae704855f0a67ab
Sai Chowdary <saichowdary4081@gmail.com>
2026-08-22 13:38:30 +0530

    Add SQL queries for subquery tasks

--- Week-3-SQL-Excel/Week-3-SQL-Excel/Task-05-SQL-Subqueries/task5_subqueries.sql
+++ Week-3-SQL-Excel/Week-3-SQL-Excel/Task-05-SQL-Subqueries/task5_subqueries.sql
@@ -1,1 +1,47 @@
+-- Task 5: SQL Subqueries
 
+
+-- Problem 1:
+-- Find employees earning more than the average salary
+
+SELECT
+    Employee_ID,
+    Name,
+    Department,
+    Salary
+FROM Employees
+WHERE Salary > (
+    SELECT AVG(Salary)
+    FROM Employees
+);
+
+
+-- Problem 2:
+-- Find employees earning the highest salary
+
+SELECT
+    Employee_ID,
+    Name,
+    Department,
+    Salary
+FROM Employees
+WHERE Salary = (
+    SELECT MAX(Salary)
+    FROM Employees
+);
+
+
+-- Problem 3:
+-- Find employees working in the same department as Rahul
+
+SELECT
+    Employee_ID,
+    Name,
+    Department,
+    Salary
+FROM Employees
+WHERE Department = (
+    SELECT Department
+    FROM Employees
+    WHERE Name = 'Rahul'
+);
